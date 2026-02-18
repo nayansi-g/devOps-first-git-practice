9fae18add5e7b7ab0284ea7fb2a1293b66055802
nayansi-g <nayansigupta29@gmail.com>
2026-02-19 03:31:42 +0530

    changes again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"devOps-first-git-practice",
+  base:"/devOps-first-git-practice/",
   plugins: [react()],
 })
